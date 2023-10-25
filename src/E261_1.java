049c7db95f421d3b98164e1377276803b19c6920
u-toru42 <golt7040@gmail.com>
2023-10-25 20:45:32 +0900

    練習2-6-1の回答

---
+++ src/E261_1.java
@@ -0,0 +1,10 @@
+public class E261_1 {
+        public static void main(String[] args) {
+            int num = 10;
+            double value = 2.5;
+            System.out.println("" + num + "/3");
+            System.out.println(num + 1 + "/3");
+            System.out.println("答えは" + num + value);
+            System.out.println("答えは" + ((double)num + value));
+        }
+}
